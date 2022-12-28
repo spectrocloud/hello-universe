26fbd01f8f0cf097824f4565b87acde4d7764ff8
Karl Cardenas <karl@spectrocloud.com>
2022-12-28 15:43:01 -0700

    fix: fixed size issue on mobile

--- src/utilities/helpers.js
+++ src/utilities/helpers.js
@@ -7,17 +7,13 @@ import logoThree from "../img/spectronaut_balloons.png";
 function randomLogo(firstLoad) {
     const logos = [logo, logoOne, logoTwo, logoThree];
     const randomIndex = Math.floor(Math.random() * logos.length);
-    const imageSize = {
-        1: [500, 600],
-        2: [400, 500],
-        3: [150, 800]
-    }
+
 
     if (firstLoad || randomIndex === 0) {
         return <img src={logo} className="App-logo" alt="logo" />
     }
 
-    return  <img src={logos[randomIndex]} className="App-logo" alt="logo" width={imageSize[randomIndex][0]} height={imageSize[randomIndex][1]} />
+    return  <img src={logos[randomIndex]} className="App-logo" alt="logo" />
 }
 
 export {randomLogo};
