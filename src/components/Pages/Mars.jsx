15410da84658ce70df549f951d1a485aa2773780
addetz <43963729+addetz@users.noreply.github.com>
2024-08-22 14:57:52 +0100

    docs: add rotating facts DOC-1138

--- src/components/Pages/Mars.jsx
+++ src/components/Pages/Mars.jsx
@@ -1,15 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Title from './Elements/Title';
 
-const Mars = () => {
+const facts = [
+  'Mars is about half the size of Earth. If Earth were the size of a nickel, Mars would be about as big as a raspberry.', 
+  'NASA missions have found evidence that Mars was much wetter and warmer, with a thicker atmosphere, billions of years ago. Life on Mars could have existed in those conditions!', 
+  'Mars\' signature colour comes from the large amount of iron oxide, or rust as you might know it, in its rocks and soil.', 
+  'Mars is home to the highest mountain in our solar system, a volcano called Olympus Mons. It\'s about three times the height of Mount Everest!', 
+  'A year on Mars lasts 687 Earth days! This is because Mars take a lot longer than Earth to complete its orbit around the Sun.',
+  'Mars is one of the most explored planets in our solar system, and it\'s the only planet where we\'ve sent rovers to roam the alien landscape.', 
+  'Mars has two moons, Phobos and Deimos.',
+];
+
+const INTERVAL_LENGTH = 5000;
+
+class Mars extends Component {
+  
+  constructor() {
+    super();
+    this.state = { factIndex: 0 };
+  }
+
+  componentDidMount() {
+    this.timeout = setInterval(() => {
+      let currentIdx = this.state.factIndex;
+      this.setState({ factIndex: currentIdx + 1 });
+    }, INTERVAL_LENGTH);
+  }
+
+  componentDidUnmount() {
+    clearInterval(this.timeout);
+  }
+
+  render() {
+    let fact = facts[this.state.factIndex % facts.length];
+
     return (
       <div className="Header-items">
-      <Title title = {`Mars`} 
-        subtitle={
-        `With a radius of 2,106 miles (3,390 kilometers), Mars is about half the size of Earth.  If Earth were the size of a nickel, Mars would be about as big as a raspberry.`  
-        }/>
-    </div>
-    );
+        <Title title = {`Mars`} 
+          subtitle={fact}/>
+      </div>
+      );
+    }
   };
   
   export default Mars;
--- src/components/Pages/Moon.jsx
+++ src/components/Pages/Moon.jsx
@@ -1,15 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Title from './Elements/Title';
 
-const Moon = () => {
+const facts = [
+  'The Moon was likely formed after a Mars-sized body collided with Earth about 4.5 billion years ago.', 
+  'The Moon has a day side and a night side, which change as it rotates. However, we always see the same face from Earth.', 
+  'The Moon is Earth\'s only permanent natural satellite. It is also the fifth largest sattelite in the Solar system.', 
+  'The Moon is 400 times smaller than the Sun, but also 400 times closer to Earth. This is why the Sun and Moon appear the same size when observed from Earth.',
+  'The Moon has moonquakes, which are shorter and weaker than earthquakes. They are caused by the gravitational influence of the Earth.', 
+  'There is water on the Moon! It is in the form of ice within dust and minerals on and under its surface.', 
+  'The Moon is moving approximately 1.48 inches (3.8 cm) away from Earth every year.', 
+];
+
+const INTERVAL_LENGTH = 5000;
+
+class Moon extends Component {
+  
+  constructor() {
+    super();
+    this.state = { factIndex: 0 };
+  }
+
+  componentDidMount() {
+    this.timeout = setInterval(() => {
+      let currentIdx = this.state.factIndex;
+      this.setState({ factIndex: currentIdx + 1 });
+    }, INTERVAL_LENGTH);
+  }
+
+  componentDidUnmount() {
+    clearInterval(this.timeout);
+  }
+
+  render() {
+    let fact = facts[this.state.factIndex % facts.length];
+
     return (
       <div className="Header-items">
-      <Title title = {`Earth's Moon`} 
-        subtitle={
-        `The Moon was likely formed after a Mars-sized body collided with Earth several billion years ago.Like Earth, the Moon has a day side and a night side, which change as the Moon rotates.`  
-        }/>
-    </div>
-    );
+        <Title title = {`Earth's Moon`} 
+          subtitle={fact}/>
+      </div>
+      );
+    }
+
   };
   
   export default Moon;
