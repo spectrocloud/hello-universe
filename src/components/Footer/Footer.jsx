3ccb859df934c11db530286fefed0aa19446e690
caroldelwing <carolina.delwing@spectrocloud.com>
2025-06-05 16:05:04 -0400

    feat: update colors and icons (#28)
    * feat: update colors and icons
    
    * feat: update variable name and font
    
    * docs: update gif and container image
    
    * feat: fix typo, clean up files
    
    * chore: apply suggestions from review

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import './Footer.css'
+import "./Footer.css";
 import twitter from "../../img/x.webp";
 import linkedin from "../../img/linkedin.webp";
 import mastodon from "../../img/mastodon.webp";
-import logo from "../../img/logo.svg"
+import logo from "../../img/logo.svg";
 
 const ImageInvert = React.memo(function Image({ src, altText }) {
   return <img className="Social-invert" src={src} alt={altText} />;
@@ -18,47 +18,47 @@ const ImageFooter = React.memo(function Image({ src, altText }) {
 });
 
 const Footer = () => {
-    return(
-        <footer className="footer">
-          <div className="Social-icons">
-            <a
-              href="https://www.linkedin.com/company/spectro-cloud/"
-              target={"_blank"}
-              rel={"noreferrer"}
-            >
-              <ImageInvert src={linkedin} altText={"linkedin"} />
-            </a>
+  return (
+    <footer className="footer">
+      <div className="Social-icons">
+        <a
+          href="https://www.linkedin.com/company/spectro-cloud/"
+          target={"_blank"}
+          rel={"noreferrer"}
+        >
+          <ImageInvert src={linkedin} altText={"linkedin"} />
+        </a>
 
-            <a
-              href="https://x.com/spectrocloudinc"
-              target={"_blank"}
-              rel={"noreferrer"}
-            >
-              <Image src={twitter} alt="twitter" />
-            </a>
+        <a
+          href="https://x.com/spectrocloudinc"
+          target={"_blank"}
+          rel={"noreferrer"}
+        >
+          <Image src={twitter} alt="twitter" />
+        </a>
 
-            <a
-              href="https://hachyderm.io/@spectrocloudinc"
-              target={"_blank"}
-              rel={"noreferrer"}
-            >
-              <ImageInvert src={mastodon} altText={"mastodon"} />
-            </a>
-          </div>
-          <div>
-            <div>
-            <span className="Footer-rights">
-              Spacetastic Ltd. is a fictional company used by Spectro Cloud as an example company and domain. 
-              <br />
-              © 2024 Spectro Cloud®. All rights reserved.
-            </span>
-            </div>
-            <div className="footer-logo-container">
-              <ImageFooter src={logo} alt="spectrocloud" />
-            </div>
-          </div>
-        </footer>        
-    );
-}
+        <a
+          href="https://hachyderm.io/@spectrocloudinc"
+          target={"_blank"}
+          rel={"noreferrer"}
+        >
+          <ImageInvert src={mastodon} altText={"mastodon"} />
+        </a>
+      </div>
+      <div>
+        <div>
+          <span className="Footer-rights">
+            Spacetastic Ltd. is a fictional company used by Spectro Cloud as an
+            example company and domain.
+            <br />© 2025 Spectro Cloud®. All rights reserved.
+          </span>
+        </div>
+        <div className="footer-logo-container">
+          <ImageFooter src={logo} alt="spectrocloud" />
+        </div>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
--- src/components/Pages/Elements/PlanetSelection.jsx
+++ src/components/Pages/Elements/PlanetSelection.jsx
@@ -1,21 +1,30 @@
-import React from 'react';
+import React from "react";
 import "./Elements.css";
 
 const PlanetSelection = () => {
   return (
     <div className="selection-container">
-      <div className="selection-text">Pick a space destination you want to learn about. 👇 </div>
+      <div className="selection-text">
+        Pick a space destination you want to learn about. 👇
+      </div>
       <div className="selection-all-button-container">
         <div className="selection-button-container">
-          <a className="selection-button" href="/moon">Earth's Moon</a>
+          <a className="selection-button" href="/moon">
+            Earth's Moon
+          </a>
         </div>
         <div className="selection-button-container">
-          <a className="selection-button" href="/mars">Mars</a>
+          <a className="selection-button" href="/mars">
+            Mars
+          </a>
         </div>
       </div>
       <div className="description-text">
-        Curious about how popular our space destinations are? Check out the  
-        <a className= "description-link" href="/stats"> Spacetastic Site Statictics</a> page.
+        Curious about how popular our space destinations are? Check out the{" "}
+        <a className="description-link" href="/stats">
+          Spacetastic Site Statistics
+        </a>{" "}
+        page.
       </div>
     </div>
   );
